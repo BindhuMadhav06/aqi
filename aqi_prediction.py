556689ea177d3e58e4429e4f8dfd60af14c9ff58
BindhuMadhav06 <bindhumadhav2006@gmail.com>
2025-07-09 15:56:06 +0530

    Your commit message

--- aqi_prediction.py
+++ aqi_prediction.py
@@ -11,6 +11,7 @@ import io
 import base64
 from flask import Flask, render_template, jsonify, request, send_file
 from datetime import datetime, timedelta
+from aqi import calculate_aqi  
 import threading
 import time
 import logging
